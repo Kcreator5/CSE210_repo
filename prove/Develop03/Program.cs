eaa86101ccc0be31a0f58e6738917b2fc4d7f33c
Kcreator5 <kevinwilliamrogers@gmail.com>
2025-06-01 19:26:14 -0600

    sinc

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,7 +8,7 @@ class Program
         Blanking blanking = new Blanking();
         // Console.WriteLine("");
         // Console.WriteLine("Enter a sentence or paragraph:");
-        
+
         // string input = Console.ReadLine();
 
         verse.SetOriginalText("2 And I seal up these records, after I have spoken a few words by way of exhortation unto you. Moroni 10:2 "); // 'input'
