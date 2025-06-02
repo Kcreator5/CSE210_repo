46944763a5426307c228fb304e3cd1dd0ffd71af
Kevin Rogers <131797662+Kcreator5@users.noreply.github.com>
2025-06-01 18:49:29 -0600

    changeing to one verse

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,24 +6,25 @@ class Program
     {
         Verse verse = new Verse();
         Blanking blanking = new Blanking();
-        Console.WriteLine("2 And I seal up these records, after I have spoken a few words by way of exhortation unto you. Moroni 10:2 ");
-        Console.WriteLine("Enter a sentence or paragraph:");
+        // Console.WriteLine("");
+        // Console.WriteLine("Enter a sentence or paragraph:");
         
-        string input = Console.ReadLine();
+        // string input = Console.ReadLine();
 
-        verse.SetOriginalText(input);
+        verse.SetOriginalText("2 And I seal up these records, after I have spoken a few words by way of exhortation unto you. Moroni 10:2 "); // 'input'
+
         Console.Clear();
 
         while (!verse.AllWordsBlanked())
         {
             Console.WriteLine(verse.GetDisplayText());
-            Console.WriteLine("\nPress [Enter] to blank more words...");
+            Console.WriteLine("\n Press {Enter} to blank three random words");
             Console.ReadLine();
             blanking.BlankRandomWords(verse, 3);
             Console.Clear();
         }
 
         Console.WriteLine(verse.GetDisplayText());
-        Console.WriteLine("\nAll words have been blanked out!");
+        Console.WriteLine("\n All of the words have been blanked out");
     }
 }
