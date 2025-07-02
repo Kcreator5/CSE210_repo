b89085ea9cc6264d6b963bc56d26f5aa071af218
Kcreator5 <kevinwilliamrogers@gmail.com>
2025-07-01 18:54:33 -0600

    changing changes

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,5 +1,49 @@
 using System;
 
+
+class menu
+{ 
+    public static void RunMenu()
+{
+    bool running = true;
+
+    while (running)
+    {  
+        Console.WriteLine("\nWelcome back to your journal!");
+
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine("[1] Write an entry");
+        Console.WriteLine("[2] Start with a prompt");
+        Console.WriteLine("[3] View past entries");
+        Console.WriteLine("[4] Exit the program");
+        Console.WriteLine("-----------------------------------");
+        Console.Write("Enter your choice: ");
+        string input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "1":
+                WriteEntry.WritingEntry();
+                break;
+            case "2":
+                PromptEntry.PromptingEntry();
+                break;
+            case "3":
+                LoadEntries.LoadEntriesByDate(); 
+                break;
+            case "4":
+                running = false;
+                Console.WriteLine("Goodbye!");
+                break;
+            default:
+                Console.WriteLine("Invalid input. Try again.");
+                break;
+        }
+    }
+}
+}
+
 class Program
 {
     static void Main(string[] args)
